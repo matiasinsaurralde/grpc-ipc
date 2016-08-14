db38265a0e4b5991b083873a8df16d2c630798df
Matias Insaurralde <matias@insaurral.de>
2016-08-14 08:15:01 -0400

    Picking a random worker (for fun)

--- pool.go
+++ pool.go
@@ -2,16 +2,23 @@ package main
 
 import(
   "log"
+  "math/rand"
+  "time"
 )
 
 // Pool holds the "worker" grpc servers
 type Pool struct {
   Workers []*Worker
+  Cmd []string
 }
 
+// NewPool initializes a new pool of workers.
 func NewPool(poolSize int, poolCmd []string, portRange []int) *Pool {
   log.Println("Initializing pool")
-  pool := &Pool{}
+
+  rand.Seed(time.Now().Unix())
+
+  pool := &Pool{ Cmd: poolCmd }
   pool.Workers = make([]*Worker, poolSize)
   for i := 0; i < poolSize; i++ {
     pool.Workers[i] = &Worker{ pool: pool }
@@ -19,10 +26,20 @@ func NewPool(poolSize int, poolCmd []string, portRange []int) *Pool {
   return pool
 }
 
+// Start will start all the workers.
 func(p *Pool) Start() {
   log.Println("Starting pool", p)
 
   for index, worker := range p.Workers {
     log.Println("  Starting worker:", worker, index)
+    worker.Prepare()
+    worker.Start()
   }
+}
+
+// PickWorker will return a random worker from the pool.
+func(p *Pool) PickWorker() (*Worker) {
+  index := rand.Int() % len(p.Workers)
+  log.Println("Pick worker #", index)
+  return p.Workers[index]
 }
