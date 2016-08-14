4ba164042cf3fec6a84fcea018a23d93155663da
Matias Insaurralde <matias@insaurral.de>
2016-08-14 03:24:19 -0400

    Pool/workers initialization

--- pool.go
+++ pool.go
@@ -1,5 +1,28 @@
 package main
 
+import(
+  "log"
+)
+
 // Pool holds the "worker" grpc servers
 type Pool struct {
+  Workers []*Worker
+}
+
+func NewPool(poolSize int, poolCmd []string, portRange []int) *Pool {
+  log.Println("Initializing pool")
+  pool := &Pool{}
+  pool.Workers = make([]*Worker, poolSize)
+  for i := 0; i < poolSize; i++ {
+    pool.Workers[i] = &Worker{ pool: pool }
+  }
+  return pool
+}
+
+func(p *Pool) Start() {
+  log.Println("Starting pool", p)
+
+  for index, worker := range p.Workers {
+    log.Println("  Starting worker:", worker, index)
+  }
 }
