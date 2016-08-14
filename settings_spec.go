ff1dc3def3eb8b9b8d8db8be32f3e9a0269ce762
Matias Insaurralde <matias@insaurral.de>
2016-08-14 02:52:07 -0400

    Appending pool settings

--- settings_spec.go
+++ settings_spec.go
@@ -1,6 +1,12 @@
 package main
 
 type SettingsSpec struct {
-  PoolSize  int  `json:"pool_size"`
-  Listen  string `json:"listen_port"`
+  ListenAddress  string `json:"listen_address"`
+  Pool  PoolSpec `json:"pool"`
+}
+
+type PoolSpec struct {
+  PoolSize  int `json:"pool_size"`
+  PoolCmd []string  `json:"pool_cmd"`
+  PoolPortRange []int `json:"pool_port_range"`
 }
